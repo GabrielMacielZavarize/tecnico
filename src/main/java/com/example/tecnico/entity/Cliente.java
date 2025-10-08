f22a4a70a7413963686c04a13085482b3234e60d
Gabriel Maciel Zavarize <gabrielmzavarize@gmail.com>
2025-10-08 19:37:22 -0300

    Apenas para teste

---
+++ src/main/java/com/example/tecnico/entity/Cliente.java
@@ -0,0 +1,17 @@
+package com.example.tecnico.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente extends Pessoa {
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Chamado> chamados = new ArrayList<>();
+
+    public List<Chamado> getChamados() { return chamados; }
+    public void setChamados(List<Chamado> chamados) { this.chamados = chamados; }
+}
